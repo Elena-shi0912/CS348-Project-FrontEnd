001312152e55d833e4e9864323932c8fb05fb283
Yilin Mo <moyilin276@gmail.com>
2022-11-13 17:33:47 -0500

    button change

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import Axios from "axios";
 
 function App() {
-  const [userName, setUserName] = useState("");
   const [userEmail, setUserEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const submitUserApplicationU = () => {
     Axios.post("http://localhost:3001/api/insert", 
-              {userName: userName, 
-              userEmail: userEmail,
+              {userEmail: userEmail, 
+              password: password,
               isDriver: false
               }).then((response) => { console.log(response) })};
 
   const submitUserApplicationD = () => {
     Axios.post("http://localhost:3001/api/insert", 
-              {userName: userName, 
-              userEmail: userEmail,
+              {userEmail: userEmail, 
+              password: password,
               isDriver: true
               }).then((response) => { console.log(response) })};
 
@@ -27,15 +27,15 @@ function App() {
         <hr/>
       <div></div>
       <div className="ApplicationForm">
-        <label>User Name:</label>
-        <input type = "text" name = "userName"
-               onChange = {(e) => {
-                setUserName(e.target.value);
-               }} />
         <label>User Email:</label>
         <input type = "text" name = "userEmail"
                onChange = {(e) => {
                 setUserEmail(e.target.value);
+               }} />
+        <label>Password:</label>
+        <input type = "text" name = "password"
+               onChange = {(e) => {
+                setPassword(e.target.value);
                }} />
         <button onClick={submitUserApplicationU}>Submit As User</button>
         <button onClick={submitUserApplicationD}>Submit As Driver</button>
