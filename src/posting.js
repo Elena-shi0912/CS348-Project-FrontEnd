4e1698612ffa657c41f86d5692c982ac0bf721b2
Yilin Mo <moyilin276@gmail.com>
2022-11-16 20:16:36 -0500

    syntax change

--- src/posting.js
+++ src/posting.js
@@ -5,42 +5,37 @@ import Post from "./post.js";
 
 export default function Posting() {
     const [isLoading, setLoading] = useState(true);
-    const [getposts, setGetposts] = useState();
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      loadPosts();
-   };
+    const [getposts, setGetposts] = useState([]);
 
-   const loadPosts = () => {
-    Axios.post("http://localhost:3001/api/dbinfo", 
-    {info: "I need post"}).then((response) => 
-    { 
-      setGetposts(response)
-      setLoading(false)
-    });
-   };
 
-    const posts = getposts.map(item => {
-      return (
-        <Post
-        post_id={item.post_id}
-        pickup_location={item.pickup_location}
-        dropoff_location={item.dropoff_location}
-        pickup_time={item.pickup_time}
-        available_seats={item.available_seats}
-        price_per_seat={item.price_per_seat}
-        additional_info={item.additional_info}
-        />
-      )
-    })
+    useEffect(() => {
+      Axios.get("http://localhost:3001/api/dbinfo").then((response) => 
+      { 
+        setGetposts(response.data)
+        setLoading(false)
+      });
+     }, []);  
+
+    console.log(getposts);
 
     if (isLoading) {
-      return <div className="loading">Loading...</div>;
+      return <div> Still Loading... </div>
     }
 
   return (
     <div className="posting">
-        {posts}
+        {
+          getposts.map((item)=>{
+            return <Post
+             post_id={item.post_id}
+             pickup_location={item.pickup_location}
+             dropoff_location={item.dropoff_location}
+             pickup_time={item.pickup_time}
+             available_seats={item.available_seats}
+             price_per_seat={item.price_per_seat}
+             additional_info={item.additional_info}
+            />
+          })}
     </div>
   );
 }
