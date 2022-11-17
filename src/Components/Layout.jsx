ce971668dbf7c795315b694b97c6f62af9f50a18
kldxbjz <wzczsgdsg@gmail.com>
2022-11-16 22:47:28 -0500

    Update the router accordingly to deletion of login

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Signup.jsx";
-import Login from "./Login.jsx";
 import Posting from "./Posting.jsx";
 import Signin from "./Signin.jsx";
 
@@ -8,8 +7,7 @@ export default function Layout() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
                 <Route path="/posting" element={<Posting />} />
                 <Route path="/signin" element={<Signin />} />
             </Routes>
