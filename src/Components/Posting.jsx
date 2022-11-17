20e1e1b66845f676fa8c522b6ce545999406dc4e
kldxbjz <wzczsgdsg@gmail.com>
2022-11-17 17:05:04 -0500

    Finished the css implementation of postings and posts

--- src/Components/Posting.jsx
+++ src/Components/Posting.jsx
@@ -1,5 +1,6 @@
-import React, {useState,  useEffect} from "react";
-import './Posting.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Posting.css";
 import Axios from "axios";
 import Post from "./Post.jsx";
 
@@ -7,33 +8,121 @@ export default function Posting() {
     const [isLoading, setLoading] = useState(true);
     const [getposts, setGetposts] = useState([]);
 
-
     useEffect(() => {
-      Axios.get("http://localhost:3001/api/dbinfo").then((response) => 
-      { 
-        setGetposts(response.data)
-        setLoading(false)
-      });
-     }, []);  
+        Axios.get("http://localhost:3001/api/dbinfo").then((response) => {
+            setGetposts(response.data);
+            setLoading(false);
+        });
+    }, []);
 
     if (isLoading) {
-      return <div> Still Loading... </div>
+        return <div> Still Loading... </div>;
     }
 
-  return (
-    <div className="posting">
-        {
-          getposts.map((item)=>{
-            return <Post
-             post_id={item.post_id}
-             pickup_location={item.pickup_location}
-             dropoff_location={item.dropoff_location}
-             pickup_time={item.pickup_time}
-             available_seats={item.available_seats}
-             price_per_seat={item.price_per_seat}
-             additional_info={item.additional_info}
-            />
-          })}
-    </div>
-  );
+    return (
+        <div className="main">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        Navbar
+                    </a>
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        class="collapse navbar-collapse"
+                        id="navbarSupportedContent"
+                    >
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a
+                                    class="nav-link active"
+                                    aria-current="page"
+                                    href="#"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a
+                                    class="nav-link dropdown-toggle"
+                                    href="#"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            Action
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            Another action
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <hr class="dropdown-divider" />
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            Something else here
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled">Disabled</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input
+                                class="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button
+                                class="btn btn-outline-success"
+                                type="submit"
+                            >
+                                Search
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+            <div className="posting">
+                {getposts.map((item) => {
+                    return (
+                        <Post
+                            post_id={item.post_id}
+                            pickup_location={item.pickup_location}
+                            dropoff_location={item.dropoff_location}
+                            pickup_time={item.pickup_time}
+                            available_seats={item.available_seats}
+                            price_per_seat={item.price_per_seat}
+                            additional_info={item.additional_info}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
